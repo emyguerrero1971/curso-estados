e38d17287a16f6df3b26509b287a5dc25f15d533
Luis Emilio Guerrero González <emiguerrero2006@yahoo.es>
2023-04-12 16:18:43 -0500

    Diseño listo

---
+++ lib/pages/pagina2_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+class Pagina2Page extends StatelessWidget {
+
+  const Pagina2Page({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pagina 2'),
+      ),
+      body: Center(
+         child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              MaterialButton(
+                color: Colors.blue,
+                onPressed: () {
+                  
+                },
+                child: const Text('Establecer Usuario', style: TextStyle(color: Colors.white),),
+              ),
+              const SizedBox(height: 10.0,),
+              MaterialButton(
+                color: Colors.blue,
+                onPressed: () {
+                  
+                },
+                child: const Text('Cambiar Edad', style: TextStyle(color: Colors.white),),
+              ),
+              const SizedBox(height: 10.0,),
+              MaterialButton(
+                color: Colors.blue,
+                onPressed: () {
+                  
+                },
+                child: const Text('Añadir Profesión', style: TextStyle(color: Colors.white),),
+              ),
+            ],
+         )
+      ),
+   );
+  }
+}
